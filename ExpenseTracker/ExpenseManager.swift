e93452ab9d5e1b526340fbfcc6052b66f6cd2528
Ben Howlett <ben@benhowlett.com>
2023-02-03 08:45:56 -0700

    Added func to remove expense using UUID

--- ExpenseTracker/ExpenseManager.swift
+++ ExpenseTracker/ExpenseManager.swift
@@ -13,4 +13,12 @@ class ExpenseManager: ObservableObject {
     func addExpense(_ expense: Expense) {
         expenses.append(expense)
     }
+    
+    func removeExpense(atID id: UUID) {
+        for _ in expenses {
+            if let index = expenses.firstIndex(where: {$0.id == id}) {
+                expenses.remove(at: index)
+            }
+        }
+    }
 }
